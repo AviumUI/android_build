a406cc3d7003f8a53d98bbbdbba730e29d01754e
finish0314 <finish21190@gmail.com>
2025-08-25 13:50:26 +0800

    envsetup: Default to disable roomservice
    When using your own device tree,
    the LineageOS official repository may already support the device,
    and roomservice will download it from the LOS official source.
    
    If you want to use roomservice during lunch,
    please manually set the environment variable ENABLE_SERVICE to true
    
    Change-Id: I1ec7e0ea75507fd8a6b6284a1fb5d5d66d46ecc0

--- envsetup.sh
+++ envsetup.sh
@@ -585,6 +585,7 @@ function lunch()
         fi
     fi
 
+    if [[ $ENABLE_SERVICE = "true" ]];then
     if ! check_product $product $release
     then
         # if we can't find a product, try to grab it off the LineageOS GitHub
@@ -598,6 +599,12 @@ function lunch()
         cd $T > /dev/null
         vendor/lineage/build/tools/roomservice.py $product true
         cd - > /dev/null
+    fi
+    else
+    cd $T > /dev/null
+    vendor/lineage/build/tools/gen_build-manifest.sh
+    cd - > /dev/null
+    check_product $product $release
     fi
 
     # Validate the selection and set all the environment stuff
